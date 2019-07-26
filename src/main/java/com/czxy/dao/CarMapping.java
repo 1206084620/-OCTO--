063991519d8ce9c68e6b841e6f1d80dd7edc43c4
zhangxin <1206084620@qq.com>
2019-07-26 09:04:11 +0800

    11111

---
+++ src/main/java/com/czxy/dao/CarMapping.java
@@ -0,0 +1,15 @@
+package com.czxy.dao;
+
+import com.czxy.domain.Car;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author lenovo
+ * @version v 1.0
+ * @date 2019/7/25
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface CarMapping extends Mapper<Car> {
+
+
+}
